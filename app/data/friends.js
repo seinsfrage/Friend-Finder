e7bc112a03ff64fb8d0283d80865f92198d6e5e8
Daniel Smith <docsmith.ut@gmail.com>
2016-12-11 05:53:24 -0600

    recommit

---
+++ app/data/friends.js
@@ -0,0 +1,56 @@
+//friend data
+var friendsobj = [
+	{
+		name:"Robb Stark",
+		photo:"http://images6.fanpop.com/image/photos/34200000/got-game-of-thrones-34230157-500-500.png",
+		scores:["1","1","1","1","1","1","1","1","1","1"]
+	},
+	{
+		name:"Jhon Snow",
+		photo:"http://winteriscoming.net/wp-content/uploads/2015/01/Jon-Snow-Hair-GIF-Game-of-Thrones.gif",
+		scores:["2","2","2","2","2","2","2","2","2","2"]
+	},
+	{
+		name:"Daenerys",
+		photo:"https://s-media-cache-ak0.pinimg.com/564x/8a/18/16/8a1816723ba72d5385f6d89c19d65db2.jpg",
+		scores:["3","3","3","3","3","3","3","3","3","3"]
+	},
+	{
+		name:"Tyrion",
+		photo:"http://www.femalefirst.co.uk/image-library/square/500/g/game-of-thrones-season-6-first-look-picture-tyrion.jpg",
+		scores:["4","4","4","4","4","4","4","4","4","4"]
+	},
+	{
+		name:"Joffrey",
+		photo:"http://cdn.idigitaltimes.com/sites/idigitaltimes.com/files/styles/embed/public/2014/08/28/2014/01/17/14019.jpg",
+		scores:["5","5","5","5","5","5","5","5","5","5"]
+	},
+	{
+		name:"Ygritte",
+		photo:"https://shwetasdiaries.files.wordpress.com/2015/10/c__data_users_defapps_appdata_internetexplorer_temp_saved-images_got-game-of-thrones-29950776-500-500.png",
+		scores:["1","2","1","2","1","2","1","2","1","2"]
+	},
+	{
+		name:"KHAL-Drogo",
+		photo:"http://images6.fanpop.com/image/photos/38900000/Khal-Drogo-game-of-thrones-38904151-500-500.gif",
+		scores:["2","1","2","1","2","1","2","1","2","1"]
+	},
+	{
+		name:"Sansa",
+		photo:"http://66.media.tumblr.com/10abd696328b3efc6243add40f63dd41/tumblr_inline_nmm48kWfqv1shq1o2_500.jpg",
+		scores:["3","2","1","3","2","1","3","2","1","3"]
+	},
+	{
+		name:"Arya",
+		photo:"https://media.giphy.com/media/fmd46gcrNQJePMU6xa/giphy.gif",
+		scores:["2","2","3","1","2","2","1","2","3","2"]
+	},
+	{
+		name:"Margaery",
+		photo:"http://images5.fanpop.com/image/photos/30300000/Game-Of-Thrones-Margaery-Tyrell-natalie-dormer-30399439-500-500.png",
+		scores:["5","1","4","5","2","3","2","5","5","5"]
+	},
+	
+];
+// make it accessible to other files using require. 
+module.exports = friendsobj;
